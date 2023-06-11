551e73c8cec2dd7afb5d241caaf9bc809e9a4c7e
KyungseoJung <junggs1245@gmail.com>
2023-06-11 23:15:06 +0900

    feat(Coin Block, Item Block) : Add effects of coinUi and 100pointsUi appearing in each block #3

--- Assets/SuperMarioBros3/02.Scripts/Block.cs
+++ Assets/SuperMarioBros3/02.Scripts/Block.cs
@@ -7,7 +7,7 @@ public class Block : MonoBehaviour  // 물음표 블록
     private enum BLOCK_TYPE {COIN = 1, ITEM };      // 블록 타입
     private BLOCK_TYPE blockType;
     
-// #2 코인 업다운
+// #2 블록 업다운
     private Vector3 startPos;
     private Vector3 destPos;
     private float moveTimer = 0f;
@@ -16,11 +16,16 @@ public class Block : MonoBehaviour  // 물음표 블록
 
     AnimationCurve curve = AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);   // 커브 처리를 이용해 업 다운 적용
 
-// 코인 부딪힐 때
+// 블록 부딪힐 때
     public Sprite brokenBlock;  // 부숴진 블록 이미지
     private bool isTouched = false; // 플레이어 headCheck와 블록이 닿았는지 확인용
     public AudioClip[] blockClips; // 블록 부딪힐 때 사운드  // 1번째 사운드 : 코인 사운드, 2번째 블록 사운드 : 아이템 블록 사운드  
-    
+
+// 코인 or 아이템 등장
+    public GameObject coinUi;   // #3 코인 UI가 사라진 뒤에 자동으로 100pointsUi 등장함 (애니메이터에서 설정함)
+    public GameObject mushroom;
+    public GameObject leaf;
+
     void Start()
     {
         startPos = transform.position;
@@ -28,14 +33,23 @@ public class Block : MonoBehaviour  // 물음표 블록
         destPos.y += 0.7f;  // 업다운 하는 높이 // y값을 따로 빼서 더해줘야 돼 - Vector3 통째로 값을 수정하는 것이 아니라
     }
     void OnTriggerEnter2D(Collider2D other)      // #2 플레이어가 머리로 박았을 때 (=플레이어와 부딪힘 && 플레이어 위로 점프 중임)
-    {
-        if(other.gameObject.tag == "HeadCheck" && !isTouched)            // 플레이어 headCheck에 부딪힌 거라면   && 아직 부숴진 상태가 아니라면
+    {       // 이 방법을 이용하면, 플레이어 스크립트에서 작성하지 않아도 되겠다~
+        if(other.gameObject.tag == "HeadCheck" && !isTouched)            // 딱 1번만 실행 //플레이어 headCheck에 부딪힌 거라면   && 아직 부숴진 상태가 아니라면
         {   
             Debug.Log("#2 HeadCheck");
-            StartCoroutine(BlockUpDown()); 
-        }
+            StartCoroutine(BlockUpDown());
 
-        // 플레이어한테 달아야 하나..? 다른 방법은 진짜 안되는 건가?
+            transform.GetComponent<SpriteRenderer>().sprite = brokenBlock;  // 딱딱한 블록 이미지로 변경
+            
+            // #3 코인 UI 등장
+            Vector3 coinPos;                // 생성 위치 설정
+            coinPos = transform.position;
+            coinPos.y += 1f;
+
+            Instantiate(coinUi, coinPos, Quaternion.identity);  // 생성
+            Debug.Log("#3 코인 UI 생성 위치 : " + coinPos);
+            
+        }
     }
     
     IEnumerator BlockUpDown()   // #2 블록 업다운 함수
@@ -50,7 +64,7 @@ public class Block : MonoBehaviour  // 물음표 블록
                 transform.localPosition = Vector3.Lerp(startPos, destPos, curve.Evaluate(moveTimer/upTimer));
             else if(moveTimer <= downTimer) //upTimer 초과이면서 downTimer 이하일 때
                 transform.localPosition = Vector3.Lerp(destPos, startPos, curve.Evaluate(moveTimer/downTimer));
-            else    // 시간이 모두 지났으면 = 업다운 마치고 원위치로 돌아온 후
+            else    // 시간이 모두 지났으면(0.4초 후에) = 업다운 마치고 원위치로 돌아온 후
             {
                 Debug.Log("#2 블록 부숴짐");
                 BlockIsBroken();
@@ -63,9 +77,9 @@ public class Block : MonoBehaviour  // 물음표 블록
             yield return null;
             // yield return new WaitForSeconds(1f);
         }
-
     }
-    void BlockIsBroken()    
+
+    void BlockIsBroken()    // #2 플레이어 headCheck와 블록 부딪힌 후 작동
     {
         switch(blockType)   // 블록 타입에 따라 다르게 작동
         {
@@ -76,8 +90,10 @@ public class Block : MonoBehaviour  // 물음표 블록
                 AudioSource.PlayClipAtPoint(blockClips[1], transform.position);
                 break;
         }
+    }
 
-        transform.GetComponent<SpriteRenderer>().sprite = brokenBlock;
-        
+    void ItemAppears()
+    {
+
     }
 }
---
+++ Assets/SuperMarioBros3/02.Scripts/Instantiator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour   // #3 특정 오브젝트를 특정 위치에 생성해주는 클래스
+{
+    public GameObject instObj;   // 생성할 오브젝트
+    public float yValueDiff =0;      // 생성할 오브젝트의 y축 위치 차이(이 스크립트가 들어있는 오브젝트 기준)
+    public Vector3 instPos;      // 최종 생성될 위치
+    public bool instOnAwake;      // 인스펙터에서 체크시 Awake() 에서 instantiateObj 오브젝트를 일정 시간 딜레이후 생성
+    public float awakeInstDelay;  // Awake() 에서 사용되는 생성을 위한 딜레이 타임
+
+    void Awake()
+    {
+        if(instOnAwake)
+        {
+            Invoke("InstantiateObj", awakeInstDelay);
+        }
+    }
+    
+    void InstantiateObj()
+    {
+        instPos = transform.position;
+        instPos.y += yValueDiff;        // 설정한 y축 위치 차이만큼 더하기(or 빼기)
+
+        Instantiate(instObj, instPos, Quaternion.identity);     //Quaternion.identity : 회전량이 0인 상태의 쿼터니언    //단위행렬이랑 비슷한 느낌~
+    }
+}
