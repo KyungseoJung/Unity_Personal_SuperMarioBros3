2cd0440bb0aeac543e398b84eeb94c3d7e7397c9
KyungseoJung <junggs1245@gmail.com>
2023-06-18 16:03:38 +0900

    feat(EnemyCtrl) : Add enemy(flower) movement on the map that appear and hide #12

--- Assets/SuperMarioBros3/02.Scripts/EnemyCtrl.cs
+++ Assets/SuperMarioBros3/02.Scripts/EnemyCtrl.cs
@@ -16,9 +16,24 @@ public class EnemyCtrl : MonoBehaviour  // #9 몬스터 움직임
     public float moveSpeed;        // 이동 속도
     private Rigidbody2D rBody;      
 
+// #12 꽃 움직임
+    private IEnumerator flowerUpCor;
+    private IEnumerator flowerDownCor;
+    private Vector3 startPos;
+    private Vector3 destPos;
+
+    private float moveTimer = 0f;           // 현재 움직임 파악
+    private float upDownTimer = 1f;       // 위로 올라와서 등장하는 데 1초 소모
+
+
+    AnimationCurve curve = AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);   // 커브 처리를 이용해 업 다운 적용  // 곡선 이용 - (0,0)에서 (1.1)로 가는 자연스러운 움직임 연출
+
     void Awake()
     {
         rBody = GetComponent<Rigidbody2D>();
+        
+        flowerUpCor = FlowerUp();       // #12
+        flowerDownCor = FlowerDown();
     }
 
     void Start()
@@ -28,6 +43,15 @@ public class EnemyCtrl : MonoBehaviour  // #9 몬스터 움직임
             case ENEMY_TYPE.GOOMBA :
             case ENEMY_TYPE.TURTLE : 
                 moveSpeed = 2f;
+                break;
+            
+            case ENEMY_TYPE.FLOWER : 
+                startPos = transform.position;  // #12 시작 위치, 도착 위치 설정
+                destPos = transform.position;
+                destPos.y += 1f;
+                
+                StartCoroutine(flowerUpCor);     // #12 꽃 움직임 시작
+
                 break;
         }
     }
@@ -52,4 +76,52 @@ public class EnemyCtrl : MonoBehaviour  // #9 몬스터 움직임
         enemyScale.x *= -1;
         transform.localScale = enemyScale;
     }
+
+    IEnumerator FlowerUp() // #12 꽃 - 위로 올라오기
+    {
+        while(true)
+        {
+            Debug.Log("#12 업 함수 실행//" + moveTimer);
+
+            if(moveTimer < upDownTimer)
+            {
+                transform.localPosition = Vector3.Lerp(startPos, destPos, curve.Evaluate(moveTimer/upDownTimer));
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.5f);
+                moveTimer = 0f;
+                StartCoroutine(FlowerDown());      // 다시 내려가도록
+
+                yield break;    // 코루틴 종료
+            }
+            moveTimer += Time.deltaTime;
+            yield return null;  // 한 프레임 대기
+        }
+    }
+
+    IEnumerator FlowerDown()    // #12 꽃 - 밑으로 내려가기
+    {
+        while(true)
+        {
+            Debug.Log("#12 다운 함수 실행" + moveTimer);
+
+            if(moveTimer < upDownTimer)
+            {
+                transform.localPosition = Vector3.Lerp(destPos, startPos, curve.Evaluate(moveTimer/upDownTimer));
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.0f);
+                moveTimer = 0f;
+                StartCoroutine(FlowerUp());    // 다시 올라오도록
+
+                yield break;    // 코루틴 종료
+            }
+            moveTimer += Time.deltaTime;
+            yield return null;  // 한 프레임 대기
+
+        }
+    }
+
 }
--- Assets/SuperMarioBros3/02.Scripts/EnemyLife.cs
+++ Assets/SuperMarioBros3/02.Scripts/EnemyLife.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EnemyLife : MonoBehaviour  // #11 적 머리 밟았을 때, 적을 죽이는 기능
 {
-    [SerializeField]
     private PlayerCtrl playerCtrl;
+    private bool isDie;             // 죽었나 확인
 
     private void Awake() 
     {
@@ -14,15 +14,21 @@ public class EnemyLife : MonoBehaviour  // #11 적 머리 밟았을 때, 적을 
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
+        if(isDie)       // 이미 죽었으면 아래 코드 실행 X
+            return;
+
         if(other.gameObject.tag == "Player")    // #11
         {
             if(playerCtrl.steppingOnEnemy)  // 만약 플레이어가 Enemy의 머리를 밟은 거라면
             {
                 Debug.Log("#11 플레이어 머리 밟힘");
+                isDie = true;
             }
-            else
-                Debug.Log("#11 플레이어랑 그냥 부딪힘");
+            // else
+            //     Debug.Log("#11 플레이어랑 그냥 부딪힘");
+
         }        
     }
 
+    
 }
