57a9eae47e6b3e5d061e22384f766f9b0adeab7c
KyungseoJung <junggs1245@gmail.com>
2023-07-04 17:57:50 +0900

    feat(Game Screen UI):Add a game screen UI that shows score, lives, time remaining, etc. #32

---
+++ Assets/SuperMarioBros3/02.Scripts/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()    
+    {
+        // #32 이 게임오브젝트가 사라지지 않도록
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+}
---
+++ Assets/SuperMarioBros3/02.Scripts/LobbyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;          // #32
+
+public class LobbyManager : MonoBehaviour   // #32  각종 사운드, (점수, 목숨, 남은 시간) UI 관리하는 클래스 생성
+{
+    void Start()
+    {
+        SceneManager.LoadScene("scStage1");        
+    }
+}
