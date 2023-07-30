9f6b2092eee3d92c9a7d4a6c733f53c247ae2666
KyungseoJung <junggs1245@gmail.com>
2023-07-30 21:56:12 +0900

    feat(Music): Set game background music #51

---
+++ Assets/SuperMarioBros3/02.Scripts/Music.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour  // #51 
+{
+
+    public AudioSource gameMusicArr;
+    public AudioClip[] audioClips;
+    /*
+    0 : mainMusic
+    1 : 
+    2 : 
+    3 : 
+    4 : 
+    5 : 
+    */
+
+    void Awake()
+    {
+        gameMusicArr = gameObject.AddComponent<AudioSource>(); // 오디오소스 없기 때문에, 추가해서 지정해줘야 함
+    }
+
+    void Start()
+    {
+        MainMusicOn();
+    }
+
+    public void MainMusicOn()
+    {   
+        gameMusicArr.Stop();
+        gameMusicArr.clip = audioClips[0];
+        gameMusicArr.Play();
+    }
+
+
+}
