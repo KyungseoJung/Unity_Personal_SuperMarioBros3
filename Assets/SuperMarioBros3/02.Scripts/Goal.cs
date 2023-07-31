dec656b8a017c84e0d7672c9315c64e52f1a2e34
KyungseoJung <junggs1245@gmail.com>
2023-07-31 15:00:08 +0900

    feat(Goal): Make goal point where items change quickly #52

---
+++ Assets/SuperMarioBros3/02.Scripts/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour   // #52 골 아이템 변경
+{
+    public enum GOAL_ITEM_TYPE {FLOWER = 1, STAR, MUSHROOM}
+    public GOAL_ITEM_TYPE goalItemType = GOAL_ITEM_TYPE.FLOWER;
+
+    void ChangeGoalItem(GOAL_ITEM_TYPE _type)   // 애니메이터에서 설정 - 골 이미지가 바뀜에 따라 변수의 값도 바뀌도록
+    {
+        goalItemType = _type;
+    }
+
+
+}
