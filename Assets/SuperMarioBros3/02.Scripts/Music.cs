a50a4914e6e412842ad06202ba051e47d93c82b5
KyungseoJung <junggs1245@gmail.com>
2023-08-03 16:52:18 +0900

    perf(Music): Set background music to run in loop #51

--- Assets/SuperMarioBros3/02.Scripts/Music.cs
+++ Assets/SuperMarioBros3/02.Scripts/Music.cs
@@ -19,6 +19,7 @@ public class Music : MonoBehaviour  // #51
     void Awake()
     {
         gameMusicArr = gameObject.AddComponent<AudioSource>(); // 오디오소스 없기 때문에, 추가해서 지정해줘야 함
+        gameMusicArr.loop = true;   // #51 보완
     }
 
     void Start()
