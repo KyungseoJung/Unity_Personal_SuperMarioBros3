6dc7e863a1d97c00ea61a302f88811ff68613b4f
KyungseoJung <junggs1245@gmail.com>
2023-09-23 11:57:36 +0900

    feat(EnemyCtrl): When the game is over, stop all enemy animations #75

--- Assets/SuperMarioBros3/02.Scripts/EnemyCtrl.cs
+++ Assets/SuperMarioBros3/02.Scripts/EnemyCtrl.cs
@@ -168,9 +168,21 @@ public class EnemyCtrl : MonoBehaviour  // #9 몬스터 움직임
 
     void FixedUpdate()
     {
-        if((enemyLife.enemystate == EnemyLife.ENEMY_STATE.DIE)  //#9 리팩터링
-            || lobbyManager.gameOver)   // #75 만약 게임오버 상태라면 - Enemy 움직임 멈추도록
+        if(enemyLife.enemystate == EnemyLife.ENEMY_STATE.DIE)  //#9 리팩터링
             return;
+
+        if(lobbyManager.gameOver)   // #75 만약 게임오버 상태라면 - Enemy 움직임 멈추도록
+        {
+            Debug.Log("//#75 EnemyCtrl - FixedUpdate에서 게임 오버 확인");
+
+            if((anim != null) && (anim.enabled))   // Enemy3의 경우 anim이 없기 때문에 에러 방지 목적으로 anim!=null 조건 추가
+            {
+                anim.enabled = false;   // #75
+                Debug.Log("//#75 EnemyCtrl - anim 멈춰");
+            }    
+
+            return;
+        }    
 
         switch(enemyType)
         {
