429cf9d2540ec1f6284bf953687ace6c8b16322c
KyungseoJung <junggs1245@gmail.com>
2023-10-19 03:09:05 +0900

    feat(Die Zone Prefab): If the player enters the DIE ZONE, let the player die #78

--- Assets/SuperMarioBros3/02.Scripts/Zone.cs
+++ Assets/SuperMarioBros3/02.Scripts/Zone.cs
@@ -7,6 +7,12 @@ public class Zone : MonoBehaviour
     public enum ZONE_TYPE { DESTROY = 1, DIE};     // ZONE의 타입: 아예 소멸시켜버리는 존, 플레이어 죽게 하는 존
     public ZONE_TYPE zoneType = ZONE_TYPE.DESTROY;
 
+    LobbyManager lobbyManager;
+
+    private void Awake()
+    {
+        lobbyManager = GameObject.Find("LobbyManager").GetComponent<LobbyManager>();    // #78 - 게임 오버 연속 실행되지 않도록 gameOver bool형 체크 목적
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         switch(zoneType)
@@ -16,7 +22,7 @@ public class Zone : MonoBehaviour
                     Destroy(col.gameObject);
                 break;
             case ZONE_TYPE.DIE:
-                if (col.gameObject.tag == "Player")
+                if ((col.gameObject.tag == "Player") && (!lobbyManager.gameOver))   // #78 게임 오버 연속으로 실행되지 않도록 gameOver bool형 체크
                     col.gameObject.GetComponent<PlayerLife>().PlayerDie();          // #78 플레이어가 DIE ZONE에 들어가면 - 플레이어 죽도록
                 break;
         }
